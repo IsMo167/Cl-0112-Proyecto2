d6be6a2081b736b70a4b252aeccb2f0ac7c1b1fe
IsMo167 <167446469+IsMo167@users.noreply.github.com>
2024-11-27 23:20:49 -0600

    Se modifica la clase MainArbol

--- MainArbol.java
+++ MainArbol.java
@@ -1,39 +1,67 @@
 //Referencia: https://javarush.com/es/groups/posts/es.3111.estructuras-de-datos-rbol-binario-en-java
+import java.util.Scanner;
 public class MainArbol {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         // Crear una instancia del árbol
         Arbol arbol = new Arbol();
 
         // Insertar nodos en el árbol
-        System.out.println("Insertando nodos...");
-        arbol.insertarNodo(5);
-        arbol.insertarNodo(14);
-        arbol.insertarNodo(6);
-        arbol.insertarNodo(1);
-        arbol.insertarNodo(9);
-        arbol.insertarNodo(7);
-        arbol.insertarNodo(4);
-        arbol.insertarNodo(10);
-        arbol.insertarNodo(3);
-        arbol.insertarNodo(2);
+        System.out.println("Insertar la cantidad de nodos que desea que tenga el árbol: ");
+        int cantidadNodos = scanner.nextInt();
 
-        // Imprimir el árbol
-        System.out.println("\nÁrbol después de insertar nodos:");
-        arbol.imprimirArbol();
+        for (int i = 0; i < cantidadNodos; i++ ){
+            System.out.println("Ingrese el valor del nodo " + (i+1) + ": ");
+            int valorNodo = scanner.nextInt();
+            arbol.insertarNodo(valorNodo);
+        }
 
-        // Eliminar un nodo
-        System.out.println("\nEliminando el nodo con valor..."); //Hacerlo interactivo, no quemado
-        arbol.eliminarNodo(5);
 
-        // Imprimir el árbol después de la eliminación
-        System.out.println("\nÁrbol después de eliminar el nodo :"); //Hacerlo interactivo, no quemado
-        arbol.imprimirArbol();
+        System.out.println("Desea imprimir el árbol a ésta instancia? (escriba true/false): ");
+        boolean mostrarArbol = scanner.nextBoolean();
+        if (mostrarArbol == true){
+            arbol.imprimirArbol();
+        }
+        else{
+            System.out.println ("Siguiente paso...");
+        }
+
+        // Eliminar nodos del árbol
+        System.out.println("Insertar la cantidad de nodos que desea eliminar del árbol: ");
+        int cantidadNodosEliminar = scanner.nextInt();
+
+        for (int i = 0; i < cantidadNodosEliminar; i++ ){
+            if(cantidadNodosEliminar <= cantidadNodos){        
+                System.out.println("Ingrese el valor a eliminar # " + (i+1));
+                int valorNodoEliminar = scanner.nextInt();
+                if(arbol.encontrarNodoPorValor (valorNodoEliminar) != null){
+                    arbol.eliminarNodo(valorNodoEliminar);
+                    System.out.println("Se elimina el nodo: " + valorNodoEliminar);
+                }
+                else{
+                    System.out.println ("No se puede eliminar un nodo que no existe.");
+                }
+            }
+            else{
+                System.out.println("Cantidad de nodos a eliminar fuera de rango.");
+            }
+        }
+
+        System.out.println("Desea imprimir el árbol a ésta instancia? (escriba true/false): ");
+        boolean mostrarArbolEliminar = scanner.nextBoolean();
+        if (mostrarArbol == true){
+            arbol.imprimirArbol();
+        }
+        else{
+            System.out.println ("Siguiente paso...");
+        }
 
         // Buscar un nodo por su valor
-        System.out.println("\nBuscando el nodo con valor...");
-        NodoArbol nodoEncontrado = arbol.encontrarNodoPorValor(7);
+        System.out.println("Ingrese el nodo que desea buscar: ");
+        int buscarNodo = scanner.nextInt();
+        NodoArbol nodoEncontrado = arbol.encontrarNodoPorValor(buscarNodo);
         if (nodoEncontrado != null) {
-            nodoEncontrado.imprimirNodo();
+            System.out.println ("Se encontró el nodo! El nodo encontrado es: " + buscarNodo);
         } else {
             System.out.println("Nodo no encontrado.");
         }
