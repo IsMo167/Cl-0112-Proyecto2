12831fa3fdae4b0aa41f9859e6f8cd573b13447f
AleG1312 <aguerrarod2003@gmail.com>
2024-11-28 20:53:23 -0600

    Documentación Interna Completa

--- NodoArbol.java
+++ NodoArbol.java
@@ -1,49 +1,45 @@
-//Referencia: https://javarush.com/es/groups/posts/es.3111.estructuras-de-datos-rbol-binario-en-java
+
+/**
+ * Clase: NodoArbol
+ * Propósito: Crear los objetos nodo con las características necesarias para poder construir un árbol a partir de estos
+ */
 public class NodoArbol {
     private int valor;               // El valor del nodo
     private NodoArbol hijoIzquierdo; // Referencia al hijo izquierdo
     private NodoArbol hijoDerecho;   // Referencia al hijo derecho
 
-    // Constructor
+    /**
+     * Constructor
+     * Propósito: Inicializar un objeto nodo con valor definido y enlace a sus dos hijos como null
+     * Parámetros de entrada:
+     *      - int valor: Valor que se le asignará al atributo this.valor
+     * Parámetros de salida:
+     *      - null
+     */
     public NodoArbol(int valor) {
         this.valor = valor;
         this.hijoIzquierdo = null;
         this.hijoDerecho = null;
     }
 
-    // Métodos para obtener el valor
+    // Setters y getters
     public int getValor() {
         return this.valor;
     }
-
-    // Métodos para establecer el valor
     public void setValor(int valor) {
         this.valor = valor;
     }
-
-    // Métodos para obtener el hijo izquierdo
     public NodoArbol getHijoIzquierdo() {
         return this.hijoIzquierdo;
     }
-
-    // Métodos para establecer el hijo izquierdo
     public void setHijoIzquierdo(NodoArbol hijoIzquierdo) {
         this.hijoIzquierdo = hijoIzquierdo;
     }
-
-    // Métodos para obtener el hijo derecho
     public NodoArbol getHijoDerecho() {
         return this.hijoDerecho;
     }
-
-    // Métodos para establecer el hijo derecho
     public void setHijoDerecho(NodoArbol hijoDerecho) {
         this.hijoDerecho = hijoDerecho;
-    }
-
-    // Método para imprimir el nodo 
-    public void imprimirNodo() {
-        System.out.println("Nodo seleccionado con valor: " + this.valor);
     }
 }
  
